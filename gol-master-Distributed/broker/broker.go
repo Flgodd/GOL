7c35756ef70ad8ad673fe98630b620bfe4eb4ea6
Flgodd <69981894+Flgodd@users.noreply.github.com>
2022-11-30 17:48:08 +0000

    Add files via upload

---
+++ gol-master-Distributed/broker/broker.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"uk.ac.bris.cs/gameoflife/stubs"
+)
+
+var BrokerWorld [][]byte
+var cTurns int
+var ImageHeight int
+var ImageWidth int
+var mutex_g sync.Mutex
+var PState bool
+var errorC = false
+var QState bool
+
+// used to call the servers, run in parallel
+func makeCall(client *rpc.Client, bh int, h int, turn int, out chan<- [][]byte) {
+	request := stubs.BrokerRequest{Bh: bh, H: h, World: BrokerWorld, TurnNumber: turn}
+	response := new(stubs.ToBrokerResponse)
+	err := client.Call(stubs.GolHandler, request, response)
+	if err != nil {
+		fmt.Println("error")
+		errorC = true
+	}
+	out <- response.SliceWorld
+}
+
+// Broker is considered to be the remote manager
+type Broker struct {
+	store []string
+}
+
+// PressP : used to pause
+func (s *Broker) PressP(req stubs.PPress, res *stubs.StringM) (err error) {
+	fmt.Println("PAUSED")
+	PState = req.Status
+	return
+}
+
+// ChangeQ : used to notify when client quits
+func (s *Broker) ChangeQ(req stubs.StringM, res *stubs.StringM) (err error) {
+	res.Msg = " q state changed"
+	fmt.Println("Client quit")
+	PState = true
+	QState = true
+	return
+}
+
+// GolCalc : Executes all turns of the Game of Life.
+func (s *Broker) GolCalc(req stubs.Request, res *stubs.Response) (err error) {
+	var turn int
+	turnInit := 0
+	if QState == true {
+		turnInit = cTurns
+
+	}
+	PState = false
+	// splits heights per server fairly
+	heights := make([]int, len(s.store))
+	for i := 0; i < ImageHeight; i++ {
+		heights[i%len(s.store)]++
+	}
+
+	for i := 0; i < len(s.store); i++ {
+		fmt.Println(s.store[i])
+	}
+
+	var clientArray []*rpc.Client
+
+	for i := 0; i < len(s.store); i++ {
+		client, _ := rpc.Dial("tcp", s.store[i])
+		clientArray = append(clientArray, client)
+	}
+
+	for turn = turnInit; turn < req.NumTurns; {
+		if PState == false {
+			if QState == true {
+				fmt.Println("Q pressed")
+			}
+			bh := 0
+			h := -1
+			var sChanW []chan [][]byte
+
+			for i := 0; i < len(s.store); i++ {
+
+				chanW := make(chan [][]byte)
+				sChanW = append(sChanW, chanW)
+
+				h += heights[i]
+
+				go makeCall(clientArray[i], bh, h, turn, sChanW[i])
+
+				if errorC == true {
+					fmt.Println("Error was true")
+				}
+
+				bh += heights[i]
+			}
+
+			NewWorld := make([][]byte, ImageHeight)
+			for i := range NewWorld {
+				NewWorld[i] = make([]uint8, ImageWidth)
+			}
+
+			index := 0
+			for i := 0; i < len(s.store); i++ {
+				v := <-sChanW[i]
+				for _, row := range v {
+					NewWorld[index] = row
+					index++
+				}
+			}
+			turn++
+
+			mutex_g.Lock()
+			BrokerWorld = NewWorld
+			cTurns = turn
+			mutex_g.Unlock()
+		}
+
+	}
+
+	res.Message = BrokerWorld
+	return
+}
+
+// Quitting : used to quit all components
+func (s *Broker) Quitting(req stubs.KPress, res *stubs.StringM) (err error) {
+	if req.Status == true {
+
+		for i := 0; i < len(s.store); i++ {
+			fmt.Println("quit")
+			client, _ := rpc.Dial("tcp", s.store[i])
+			kQuit := new(stubs.KQuit)
+			kStatus := stubs.KQuit{Status: true}
+			client.Call(stubs.Quitting, kStatus, kQuit)
+		}
+
+		res.Msg = "Quitting command has sent to factory"
+		os.Exit(0)
+	}
+
+	return
+}
+
+// DataReceive : used to receive initial data from client
+func (s *Broker) DataReceive(req stubs.Request, res *stubs.StringM) (err error) {
+	if QState == false {
+		fmt.Println("Here q false")
+		BrokerWorld = req.OgWorld
+		ImageHeight = req.ImageHeight
+		ImageWidth = req.ImageWidth
+	}
+
+	QState = false
+	res.Msg = "world received from local machine"
+	return
+
+}
+
+// GetStateB : used to send current data to the client
+func (s *Broker) GetStateB(req stubs.Request, res *stubs.SavedC) (err error) {
+	mutex_g.Lock()
+	res.Turns = cTurns
+	res.World = BrokerWorld
+	mutex_g.Unlock()
+
+	return
+}
+
+// Subscribe : used to accept any server to then send them work
+func (s *Broker) Subscribe(req stubs.Subscription, res *stubs.StringM) (err error) {
+	s.store = append(s.store, req.ServerAddress)
+	if err != nil {
+		res.Msg = "Error during subscription"
+	}
+	return
+}
+
+// QStatus : used to check if q has been pressed
+func (s *Broker) QStatus(req stubs.StringM, res *stubs.QStatus) (err error) {
+	res.Status = QState
+	return
+}
+
+func main() {
+	pAddr := flag.String("port", "8030", "Port to listen on")
+	flag.Parse()
+	rpc.Register(&Broker{store: []string{}})
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+	defer listener.Close()
+	rpc.Accept(listener)
+}
---
+++ gol-master-Distributed/server/server.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"time"
+	"uk.ac.bris.cs/gameoflife/stubs"
+)
+
+var ImageHeight int
+var ImageWidth int
+
+// GetLocalIP : used to get IP address of server
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback then display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
+// sums up the number of alive cells around a specific cell
+func countAliveCellsAroundCell(ImageHeight int, ImageWidth int, world [][]byte, x int, y int) byte {
+	sum := world[(y+ImageHeight-1)%ImageHeight][(x+ImageWidth-1)%ImageWidth] + world[(y+ImageHeight-1)%ImageHeight][(x+ImageWidth)%ImageWidth] +
+		world[(y+ImageHeight-1)%ImageHeight][(x+ImageWidth+1)%ImageWidth] + world[(y+ImageHeight)%ImageHeight][(x+ImageWidth-1)%ImageWidth] + world[(y+ImageHeight)%ImageHeight][(x+ImageWidth+1)%ImageWidth] +
+		world[(y+ImageHeight+1)%ImageHeight][(x+ImageWidth-1)%ImageWidth] + world[(y+ImageHeight+1)%ImageHeight][(x+ImageWidth)%ImageWidth] +
+		world[(y+ImageHeight+1)%ImageHeight][(x+ImageWidth+1)%ImageWidth]
+
+	return sum
+}
+
+// executes a turn of game of life
+func golLogic(world [][]byte, bh int, h int) [][]byte {
+
+	world2 := make([][]byte, ImageHeight)
+	for k := 0; k < (ImageWidth); k++ {
+		world2[k] = make([]byte, ImageWidth)
+		for i := 0; i < ImageWidth; i++ {
+			world2[k] = append(world2[k], 0x00)
+		}
+	}
+
+	for i := bh; i <= h; i++ {
+		for j := 0; j < (ImageWidth); j++ {
+
+			count := countAliveCellsAroundCell(ImageHeight, ImageWidth, world, j, i)
+			count = 255 - count + 1
+
+			if world[i][j] == 0xFF {
+				if count < 2 {
+					world2[i][j] = 0x00
+				} else if count == 2 || count == 3 {
+					world2[i][j] = 0xFF
+				} else {
+					world2[i][j] = 0x00
+				}
+			} else {
+				if count == 3 {
+					world2[i][j] = 0xFF
+				} else {
+					world2[i][j] = 0x00
+				}
+			}
+		}
+	}
+
+	return world2[bh : h+1]
+}
+
+// GameOperations structure used as factory
+type GameOperations struct{}
+
+func (s *GameOperations) GolDist(req stubs.BrokerRequest, res *stubs.ToBrokerResponse) (err error) {
+
+	if req.NumTurns < 0 {
+		err = errors.New(" A number of turns must be specified")
+		return
+	}
+	fmt.Println("Starting: ", req.TurnNumber)
+	ImageWidth = len(req.World)
+	ImageHeight = len(req.World)
+	SWorld := golLogic(req.World, req.Bh, req.H)
+	res.SliceWorld = SWorld
+	fmt.Println("Ending: ", req.TurnNumber)
+	return
+}
+
+// Quitting : used to shut down server
+func (s *GameOperations) Quitting(req stubs.KQuit, res *stubs.StringM) (err error) {
+	if req.Status == true {
+		fmt.Println("quitting")
+		res.Msg = "quitting"
+		os.Exit(0)
+	}
+	return
+}
+
+func main() {
+	pAddr := flag.String("port", "8050", "port to listen on")
+	brokerAddr := flag.String("broker", "127.0.0.1:8030", "Address of broker instance")
+	flag.Parse()
+	client, _ := rpc.Dial("tcp", *brokerAddr)
+	rand.Seed(time.Now().UnixNano())
+	rpc.Register(&GameOperations{})
+	listen, err := net.Listen("tcp", ":"+*pAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	status := new(stubs.StringM)
+	client.Call(stubs.Subscribe, stubs.Subscription{ServerAddress: GetLocalIP() + ":" + *pAddr, Callback: "Factory.Multiply"}, status)
+
+	defer listen.Close()
+
+	rpc.Accept(listen)
+	flag.Parse()
+
+}
